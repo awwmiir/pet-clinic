2b19a2527bb9b24cfc92535bca4e799d6ecbc5e2
amir.alizad <aalizad79@gmail.com>
2021-10-25 19:04:40 +0330

    Add test for findOwner method in PetManager

--- src/test/java/org/springframework/samples/petclinic/owner/PetManagerTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetManagerTest.java
@@ -3,12 +3,16 @@ package org.springframework.samples.petclinic.owner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.samples.petclinic.utility.PetTimedCache;
 import org.springframework.samples.petclinic.utility.SimpleDI;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest
 class PetManagerTest {
 	@MockBean
 	private PetTimedCache petTimedCache;
@@ -23,5 +27,13 @@ class PetManagerTest {
 		petManager = new PetManager(petTimedCache, ownerRepository, logger);
 	}
 
-
+	@Test
+	void findOwner_method_returns_the_correct_owner_with_the_requested_id(){
+		Owner expectedOwner = new Owner();
+		when(ownerRepository.findById(1)).thenReturn(expectedOwner);
+		Owner actualOwner = petManager.findOwner(1);
+		assertNotNull(actualOwner);
+		assertEquals(actualOwner, expectedOwner);
+		verify(ownerRepository).findById(1);
+	}
 }

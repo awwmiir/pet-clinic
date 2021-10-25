b3fa28849e4485558f7df27dc0c68768d6c263d8
amir.alizad <aalizad79@gmail.com>
2021-10-25 18:44:11 +0330

    Creat branch from intellij

--- src/test/java/org/springframework/samples/petclinic/owner/PetManagerTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetManagerTest.java
@@ -1,11 +1,19 @@
 package org.springframework.samples.petclinic.owner;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.samples.petclinic.utility.PetTimedCache;
 import org.springframework.samples.petclinic.utility.SimpleDI;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PetManagerTest {
-
+	@MockBean
+	private PetTimedCache petTimedCache;
+	@MockBean
+	private OwnerRepository ownerRepository;
+	@MockBean
+	private Logger logger;
 
 }

f84c14e2f085448e0279ef541ad3ea76f6611645
awwmiir <aalizad79@gmail.com>
2021-11-15 22:24:57 +0330

    Add tests for new users

---
+++ src/test/java/org/springframework/samples/petclinic/model/priceCalculators/CustomerDependentPriceCalculatorTest.java
@@ -0,0 +1,47 @@
+package org.springframework.samples.petclinic.model.priceCalculators;
+
+import org.junit.Test;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.UserType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CustomerDependentPriceCalculatorTest {
+
+	private CustomerDependentPriceCalculator customerDependentPriceCalculator;
+	private List<Pet> pets;
+
+	public static Double BASE_CHARGE = 10.0, BASE_PRICE_PER_PET = 5.0;
+
+	void setUp(){
+		customerDependentPriceCalculator = new CustomerDependentPriceCalculator();
+		pets = new ArrayList<>();
+	}
+
+	@Test
+	public void newUsersWithEmptyListsAreNotCharged(){
+		this.setUp();
+		assertEquals(
+			0,
+			customerDependentPriceCalculator.calcPrice(pets, BASE_CHARGE, BASE_PRICE_PER_PET, UserType.NEW));
+	}
+
+	@Test
+	public void silverUsersWithEmptyListsAreNotCharged(){
+		this.setUp();
+		assertEquals(
+			0,
+			customerDependentPriceCalculator.calcPrice(pets, BASE_CHARGE, BASE_PRICE_PER_PET, UserType.SILVER));
+	}
+
+	@Test
+	public void goldUsersWithEmptyListAreChargedWithBaseCharge(){
+		this.setUp();
+		assertEquals(
+			BASE_CHARGE.doubleValue(),
+			customerDependentPriceCalculator.calcPrice(pets, BASE_CHARGE, BASE_PRICE_PER_PET, UserType.GOLD));
+	}
+}

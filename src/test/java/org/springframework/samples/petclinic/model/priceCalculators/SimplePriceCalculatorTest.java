9debf6bba8f363a1764c6bf9ed4b6032c3c9b955
pkootzari <pkootzari@gmail.com>
2021-11-15 20:43:38 +0330

    create the test file

---
+++ src/test/java/org/springframework/samples/petclinic/model/priceCalculators/SimplePriceCalculatorTest.java
@@ -0,0 +1,16 @@
+package org.springframework.samples.petclinic.model.priceCalculators;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.model.UserType;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class SimplePriceCalculatorTest {
+
+}

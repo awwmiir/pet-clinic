a58ce6f384f8806a413fd92bff54a34fa9416fc6
awwmiir <aalizad79@gmail.com>
2021-11-15 22:56:48 +0330

    Modify setUp method

--- src/test/java/org/springframework/samples/petclinic/model/priceCalculators/CustomerDependentPriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/model/priceCalculators/CustomerDependentPriceCalculatorTest.java
@@ -2,9 +2,12 @@ package org.springframework.samples.petclinic.model.priceCalculators;
 
 import org.junit.Test;
 import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.model.UserType;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -13,12 +16,40 @@ public class CustomerDependentPriceCalculatorTest {
 
 	private CustomerDependentPriceCalculator customerDependentPriceCalculator;
 	private List<Pet> pets;
+	private Date birthDate;
+	private Pet pet1, pet2, pet3, pet4, pet5;
+	private PetType petType;
 
 	public static Double BASE_CHARGE = 10.0, BASE_PRICE_PER_PET = 5.0;
 
 	void setUp(){
 		customerDependentPriceCalculator = new CustomerDependentPriceCalculator();
+		petType = new PetType();
 		pets = new ArrayList<>();
+		birthDate = new Calendar.Builder()
+			.setDate(2017, 1, 1)
+			.build()
+			.getTime();
+
+		pet1 = new Pet();
+		pet1.setType(petType);
+		pet1.setBirthDate(birthDate);
+
+		pet2 = new Pet();
+		pet2.setType(petType);
+		pet2.setBirthDate(birthDate);
+
+		pet3 = new Pet();
+		pet3.setType(petType);
+		pet3.setBirthDate(birthDate);
+
+		pet4 = new Pet();
+		pet4.setType(petType);
+		pet4.setBirthDate(birthDate);
+
+		pet5 = new Pet();
+		pet5.setType(petType);
+		pet5.setBirthDate(birthDate);
 	}
 
 	@Test

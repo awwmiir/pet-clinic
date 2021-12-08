b820638489c75e07762715c964dc1248aec9fb68
awwmiir <aalizad79@gmail.com>
2021-12-08 15:54:41 +0330

    Add tests for processCreationForm method

--- src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetControllerTests.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.petclinic.owner;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(value = PetController.class,
 	includeFilters = {
@@ -37,10 +38,19 @@ class PetControllerTests {
 	private static final String PREFIX = "/owners/1/";
 	private static final String CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
 	private Owner owner;
+	private Pet pet;
+	private PetType dog;
 
 	@BeforeEach
 	public void setUp(){
 		owner = new Owner();
+		dog = new PetType();
+		dog.setName("dog");
+		pet = new Pet();
+		pet.setName("Pet1");
+		pet.setType(dog);
+		given(this.petRepository.findPetTypes()).willReturn(Lists.newArrayList(dog));
+		given(this.ownerRepository.findById(1)).willReturn(owner);
 	}
 
 	@Test
@@ -49,6 +59,31 @@ class PetControllerTests {
 		ResultActions resultActions = mvc.perform(get(PREFIX + "pets/new")
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(view().name(CREATE_OR_UPDATE_FORM));
+				.andExpect(view().name(CREATE_OR_UPDATE_FORM))
+				.andExpect(model().attributeExists("pet"));
 	}
+
+	@Test
+	public void whenThereIsNoErrorsInBindingsNewPetIsAddedAndRedirectedToTheFirstPage() throws Exception{
+		ResultActions resultActions = mvc.perform(post(PREFIX + "pets/new")
+				.param("name", "Pet1")
+				.param("type", "dog")
+				.param("id", "1")
+				.param("birthDate", "2019-11-18"))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(model().attributeDoesNotExist("pet"));
+	}
+
+//	@Test
+//	public void whenThereIsNoErrorsInBindingsNewPetIsAddedAndRedirectedToTheFirstPage1() throws Exception{
+//		owner.addPet(pet);
+//		ResultActions resultActions = mvc.perform(post(PREFIX + "pets/new")
+//				.param("name", "Pet1")
+//				.param("type", "dog")
+//				.param("id", "")
+//				.param("birthDate", "2019-11-18"))
+//				.andExpect(status().isOk())
+//				.andExpect(model().attributeExists("pet"));
+//	}
+
 }
